29703ed87d261c39ba07d032f507dad1fe05792e
Vagner Leite da Silva <vagner.leite.silva@gmail.com>
2025-08-21 15:39:37 -0300

    created container deploy action˜

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   // Your webpack configuration
   module: {
@@ -15,4 +17,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
