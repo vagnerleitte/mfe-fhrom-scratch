e8a169b035f880d243634011d668d3c7d6e2875d
Vagner Leite da Silva <vagner.leite.silva@gmail.com>
2025-08-23 11:52:14 -0300

    add components and update workflow

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            Microfront Store
+            MicroFront Store
           </Typography>
           <Button
             color="primary"
