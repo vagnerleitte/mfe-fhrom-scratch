c217208dd9fc56d8d62eb82f022cdf467c5ccedd
Vagner Leite da Silva <vagner.leite.silva@gmail.com>
2025-08-26 10:52:41 -0300

    fix: marketing app

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -7,7 +7,7 @@ const mount = (el) => {
   ReactDOM.render(<App />, el);
 };
 
-// If we are in development mode and isolation,
+// If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
